8a3cde14c9f7c45fb26337ff7a79ef9009eb8480
DasParijat <143519231+DasParijat@users.noreply.github.com>
2025-11-07 19:43:26 -0500

    Configured project to handle SFML

--- HFALL2025/Fall2025HackathonGame/Main.cpp
+++ HFALL2025/Fall2025HackathonGame/Main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include "SFML/Graphics.hpp"
 // TODO Set up SFML in project
 
+using namespace sf;
 using namespace std;
 
 int main() {
+	Vector2f testVector; // testing if sfml works
 
 	return 0;
 }
