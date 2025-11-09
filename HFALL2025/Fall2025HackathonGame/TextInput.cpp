8eaa89268d7c6b89369e364ecb5ad0627f4b26ab
DasParijat <dasparijat4705@gmail.com>
2025-11-09 16:59:24 -0500

    Updated TextInput.cpp

--- HFALL2025/Fall2025HackathonGame/TextInput.cpp
+++ HFALL2025/Fall2025HackathonGame/TextInput.cpp
@@ -11,6 +11,8 @@ TextInput::TextInput() {
     text.setCharacterSize(28);
     text.setFillColor(sf::Color::White);
     text.setPosition(15.f, 8.f);
+
+    text.setString("Enter task here");
 }
 
 
@@ -52,7 +54,7 @@ void TextInput::handleEvent(sf::Event& event) {
             // Backspace key
             content.pop_back();
         }
-        else if (event.text.unicode >= 32 && event.text.unicode < 128) {
+        else if (event.text.unicode >= 32 && event.text.unicode < 128 && content.length() <= 30) {
             // Printable characters
             content += static_cast<char>(event.text.unicode);
         }
