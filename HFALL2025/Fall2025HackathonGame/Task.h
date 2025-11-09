5482a9fe16f1c3e6986171d4a95753d74ce2f248
smasone <sophia.masone1@marist.edu>
2025-11-09 10:29:26 -0500

    Task.cpp

--- HFALL2025/Fall2025HackathonGame/Task.h
+++ HFALL2025/Fall2025HackathonGame/Task.h
@@ -8,11 +8,11 @@ private:
 	std::string title;
 	struct tm dueDate;
 	int difficulty;
-	bool isCompleted;
 	std::string note;
+	bool isCompleted = false;
 
 public:
 	Task(std::string title, int day, int month, int year, int difficulty, std::string note = "");
-	bool complete();
+	int complete();
 
 };
